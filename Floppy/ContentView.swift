8a5320fa815e6f25d5a4f97988ddffbdeb164e67
PloppyFenis <ploppyfenisinc@gmail.com>
2022-02-07 18:08:21 -0800

    Initial Commit

---
+++ Floppy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Floppy
+//
+//  Created by Vinny Lazzara on 2/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Floppy/FloppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloppyApp.swift
+//  Floppy
+//
+//  Created by Vinny Lazzara on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FloppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
